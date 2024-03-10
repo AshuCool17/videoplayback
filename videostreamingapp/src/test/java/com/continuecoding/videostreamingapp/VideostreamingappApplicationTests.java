8311592283fff288fa59b4cc237efbbd1f0b39f7
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-10 22:51:42 +0530

    Added main application & tests

---
+++ videostreamingapp/src/test/java/com/continuecoding/videostreamingapp/VideostreamingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.continuecoding.videostreamingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideostreamingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
