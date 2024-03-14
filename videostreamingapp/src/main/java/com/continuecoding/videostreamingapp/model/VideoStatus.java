2141e1a9ac70fda88ea61b4839cdb59ecd6b9ecd
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-14 22:49:59 +0530

    Added constructor

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/model/VideoStatus.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/model/VideoStatus.java
@@ -3,10 +3,17 @@
  */
 package com.continuecoding.videostreamingapp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 /**
  * @author Ashutosh
  *
  */
+@Document(value = "videoStatus")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VideoStatus {
 
 }
