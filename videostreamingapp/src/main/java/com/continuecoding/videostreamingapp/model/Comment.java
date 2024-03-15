f1f004302d400620bb4a6d40297d9676f29f2519
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-15 23:58:29 +0530

    Added document annotation

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/model/Comment.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/model/Comment.java
@@ -3,10 +3,13 @@
  */
 package com.continuecoding.videostreamingapp.model;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 /**
  * @author Ashutosh
  *
  */
+@Document(value = "comment")
 public class Comment {
 
 }
