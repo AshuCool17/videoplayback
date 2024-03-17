1a8f0779eaf1da758cfdbdde0718f8490597c7c7
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-17 22:36:35 +0530

    Removed unused imports

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/model/VideoStatus.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/model/VideoStatus.java
@@ -3,12 +3,6 @@
  */
 package com.continuecoding.videostreamingapp.model;
 
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * @author Ashutosh
  *
