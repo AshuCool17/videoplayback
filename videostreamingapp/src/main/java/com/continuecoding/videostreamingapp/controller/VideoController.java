2891aa43ec5b883d260ab9ec0f5b3097a6d7a135
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-18 23:37:59 +0530

    Created VideoController

---
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/controller/VideoController.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.continuecoding.videostreamingapp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Ashutosh
+ *
+ */
+@RestController(value = "/api/videos")
+public class VideoController {
+
+}
