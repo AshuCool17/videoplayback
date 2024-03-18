0e01a1f6ec382fa85f4824fe6e5fb3d070d35f3e
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-18 23:39:29 +0530

    Added uploadVideo Rest API

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/controller/VideoController.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/controller/VideoController.java
@@ -4,6 +4,7 @@
 package com.continuecoding.videostreamingapp.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author Ashutosh
@@ -12,4 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController(value = "/api/videos")
 public class VideoController {
 
+	@PostMapping
+	private void uploadVideo(@RequestParam("file") MultipartFile file) {
+		
+	}
 }
