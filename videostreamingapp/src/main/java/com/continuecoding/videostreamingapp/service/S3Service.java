8b41ffea7b65df40903af90e414b2ef1c746a88f
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-19 23:05:57 +0530

    Modified class

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/S3Service.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/S3Service.java
@@ -4,12 +4,19 @@
 package com.continuecoding.videostreamingapp.service;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * @author Ashutosh
  *
  */
 @Service
-public interface S3Service extends FileService {
+public class S3Service implements FileService {
 
+	@Override
+	public void uploadFile(MultipartFile file) {
+		//to-do
+	}
+
+	
 }
