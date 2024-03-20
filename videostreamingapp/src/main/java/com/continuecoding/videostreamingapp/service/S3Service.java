fa920ced4487f8c583823d4025d917d548417435
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-21 00:44:09 +0530

    Added filenameextension and key

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/S3Service.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/S3Service.java
@@ -3,8 +3,13 @@
  */
 package com.continuecoding.videostreamingapp.service;
 
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
+
+import lombok.var;
 
 /**
  * @author Ashutosh
@@ -16,6 +21,8 @@ public class S3Service implements FileService {
 	@Override
 	public void uploadFile(MultipartFile file) {
 		//to-do
+		String fileNameExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
+		String key = UUID.randomUUID().toString() + fileNameExtension;
 	}
 
 	
