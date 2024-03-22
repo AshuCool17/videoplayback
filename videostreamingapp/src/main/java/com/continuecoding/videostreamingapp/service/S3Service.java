f1c54fc007eeccf5129635a6557717fc732a9e03
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-22 22:51:03 +0530

    Added bucket name

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/S3Service.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/S3Service.java
@@ -3,31 +3,45 @@
  */
 package com.continuecoding.videostreamingapp.service;
 
+import java.io.IOException;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
-import lombok.var;
+import lombok.RequiredArgsConstructor;
 
 /**
  * @author Ashutosh
  *
  */
 @Service
+@RequiredArgsConstructor
 public class S3Service implements FileService {
 
+	private final AmazonS3Client amazonS3Client;
+	
 	@Override
-	public void uploadFile(MultipartFile file) {
+	public String uploadFile(MultipartFile file) {
 		//to-do
 		String fileNameExtension = StringUtils.getFilenameExtension(file.getOriginalFilename());
 		String key = UUID.randomUUID().toString() + fileNameExtension;
 		ObjectMetadata metadata = new ObjectMetadata();
 		metadata.setContentLength(file.getSize());
 		metadata.setContentType(file.getContentType());
+		
+		try {
+			amazonS3Client.putObject("video-streaming", key, file.getInputStream(), metadata);
+		} catch (SdkClientException | IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 
 	
