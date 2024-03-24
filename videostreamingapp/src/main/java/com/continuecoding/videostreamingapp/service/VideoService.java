bb81b4b2f1f1345b9718849820655f1c5453b4b1
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-24 16:53:28 +0530

    Added VideoRepository in VideoService

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/VideoService.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/VideoService.java
@@ -3,10 +3,12 @@
  */
 package com.continuecoding.videostreamingapp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.continuecoding.videostreamingapp.model.Video;
+import com.continuecoding.videostreamingapp.repository.VideoRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,12 +20,18 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class VideoService {
 	
-	private final FileService fileService;
+	@Autowired
+	private FileService fileService;
+	
+	@Autowired
+	private VideoRepository videoRepository;
 	
 	public void uploadVideo(MultipartFile file) {
 		String videoUrl = S3Service.uploadFile(file);
 		String video = new Video();
 		video.setVideoUrl(videoUrl);
+		
+		videoRepository.save(video);
 	}
 
 }
