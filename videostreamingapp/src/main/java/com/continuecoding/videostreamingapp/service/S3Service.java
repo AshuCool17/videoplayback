5b0817995dcf3c0ef7380e3989de8ab54adad38c
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-26 23:01:51 +0530

    Added read access to S3

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/S3Service.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/S3Service.java
@@ -12,6 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.amazonaws.SdkClientException;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
 import lombok.RequiredArgsConstructor;
@@ -39,6 +40,8 @@ public class S3Service implements FileService {
 		} catch (SdkClientException | IOException e) {
 			e.printStackTrace();
 		}
+		
+		amazonS3Client.setObjectAcl(BUCKET_NAME, key, CannedAccessControlList.PublicRead);
 		
 		return null;
 	}
