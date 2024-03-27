033d7140bc29d963afeda2542a7448a7c41ff02a
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-27 23:39:00 +0530

    Removed space

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/S3Service.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/S3Service.java
@@ -44,7 +44,6 @@ public class S3Service implements FileService {
 		}
 		
 		amazonS3Client.setObjectAcl(BUCKET_NAME, key, CannedAccessControlList.PublicRead);
-		
 		return amazonS3Client.getResourceUrl(BUCKET_NAME, key);
 	}
 
