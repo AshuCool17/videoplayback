4d750b00fb41dd4d9dfd89be4363b0f0f90ea8dc
Ashutosh <ashutosh.mahato@gmail.com>
2024-03-31 14:25:24 +0530

    Added addCorsMappings

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/WebMvcConfig.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/WebMvcConfig.java
@@ -4,12 +4,17 @@
 package com.continuecoding.videostreamingapp;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
  * @author Ashutosh
  *
  */
 @Configuration
-public class WebMvcConfig implements WenMvcConfigurer{
+public class WebMvcConfig implements WebMvcConfigurer{
 
+	public void addCorsMappings(CorsRegistry corsRegistry) {
+		
+	}
 }
