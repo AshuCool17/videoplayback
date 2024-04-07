3df6bb165e0e49adb70e37bc2c242b563b8acac8
Ashutosh <ashutosh.mahato@gmail.com>
2024-04-07 23:58:48 +0530

    Added id in VideoDto

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/dto/VideoDto.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/dto/VideoDto.java
@@ -3,11 +3,19 @@
  */
 package com.continuecoding.videostreamingapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author Ashutosh
  *
  */
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class VideoDto {
+	
+	private String id;
 
 }
