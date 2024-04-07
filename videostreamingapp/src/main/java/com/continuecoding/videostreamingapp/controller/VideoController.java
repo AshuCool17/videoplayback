576fc199c24b20b60b5f893ea979d97272a67f31
Ashutosh <ashutosh.mahato@gmail.com>
2024-04-07 23:56:51 +0530

    Added editVideoMetadata put API

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/controller/VideoController.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/controller/VideoController.java
@@ -5,6 +5,8 @@ package com.continuecoding.videostreamingapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
@@ -24,5 +26,10 @@ public class VideoController {
 	@PostMapping
 	private void uploadVideo(@RequestParam("file") MultipartFile file) {
 		videoService.uploadVideo(file);
+	}
+	
+	@PutMapping
+	editVideoMetadata(@RequestBody VideoDto videoDto){
+		
 	}
 }
