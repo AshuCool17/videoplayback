c699152e31ff7785db65fd72ea0d8b2ec040481d
Ashutosh <ashutosh.mahato@gmail.com>
2024-04-08 22:49:34 +0530

    Added videostatus & thumbnailurl

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/dto/VideoDto.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/dto/VideoDto.java
@@ -5,6 +5,8 @@ package com.continuecoding.videostreamingapp.dto;
 
 import java.util.Set;
 
+import com.continuecoding.videostreamingapp.model.VideoStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -23,5 +25,7 @@ public class VideoDto {
 	private String description;
 	private Set<String> tags;
 	private String videoUrl;
+	private VideoStatus videoStatus;
+	private String thumbnailUrl;
 
 }
