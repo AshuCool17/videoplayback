713fa15e18c2b031023caaed0eb0de1b0c2612fe
Ashutosh <ashutosh.mahato@gmail.com>
2024-04-15 23:59:06 +0530

    Added annotations

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/dto/UploadVideoResponse.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/dto/UploadVideoResponse.java
@@ -3,10 +3,17 @@
  */
 package com.continuecoding.videostreamingapp.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @author Ashutosh
  *
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UploadVideoResponse {
 
 	private String videoId;
