e880639a57af5037495270f196e085e8b6dec18f
Ashutosh <ashutosh.mahato@gmail.com>
2024-04-15 23:58:24 +0530

    Added UploadVideoResponse dto

---
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/dto/UploadVideoResponse.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.continuecoding.videostreamingapp.dto;
+
+/**
+ * @author Ashutosh
+ *
+ */
+public class UploadVideoResponse {
+
+	private String videoId;
+	private String videoUrl;
+}
