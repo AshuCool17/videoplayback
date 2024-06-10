c037c74706c94362879fedc3d96959a6b96f2b8b
Ashutosh <ashutosh.mahato@gmail.com>
2024-06-10 23:47:58 +0530

    Added jwt method

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/config/SecurityConfig.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/config/SecurityConfig.java
@@ -19,6 +19,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.and()
 			.sessionManagement()
 			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.oauth2ResourceServer()
+			.jwt();
 	}
 
 }
