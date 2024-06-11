7d41594d79713d74ea13f5d7a70bd42a1704cc8c
Ashutosh <ashutosh.mahato@gmail.com>
2024-06-11 23:52:02 +0530

    Added withAudience

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/config/SecurityConfig.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 		
 		OAuth2TokenValidator<Jwt> audienceValidator = new AudienceValidator(audience);
 		OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuer);
+		OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator(withIssuer, );
 	}
 
 }
