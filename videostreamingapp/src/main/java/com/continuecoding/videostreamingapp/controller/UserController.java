f6f2345c33545495c38ca33276e576386f1fa77f
Ashutosh <ashutosh.mahato@gmail.com>
2024-06-23 18:35:01 +0530

    Added requestmapping and fixed error

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/controller/UserController.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/controller/UserController.java
@@ -3,11 +3,15 @@
  */
 package com.continuecoding.videostreamingapp.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author Ashutosh
  *
  */
 @RestController
+@RequestMapping("/api/user")
 public class UserController {
 
 }
