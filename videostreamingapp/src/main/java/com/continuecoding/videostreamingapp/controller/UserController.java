2d741bfac41ecbaeb4c42ccae89f5fdb45a0cbe6
Ashutosh <ashutosh.mahato@gmail.com>
2024-06-24 23:52:42 +0530

    Added GetMapping

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/controller/UserController.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/controller/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/user")
 public class UserController {
 
+	@GetMapping("/register")
 	public String register() {
 		
 	}
