42a264b38a563632e36fe5c40e850f8a10ddd53d
Ashutosh <ashutosh.mahato@gmail.com>
2024-06-27 23:09:42 +0530

    Added Service annotation

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/UserRegistrationService.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/UserRegistrationService.java
@@ -3,10 +3,13 @@
  */
 package com.continuecoding.videostreamingapp.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * @author Ashutosh
  *
  */
+@Service
 public class UserRegistrationService {
 	
 	
