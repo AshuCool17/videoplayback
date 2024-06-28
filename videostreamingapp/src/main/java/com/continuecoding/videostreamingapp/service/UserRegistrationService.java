0517f6bdb23542830b7ab3ed471a15ccea810a37
Ashutosh <ashutosh.mahato@gmail.com>
2024-06-28 23:02:02 +0530

    Added fields

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/UserRegistrationService.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/UserRegistrationService.java
@@ -5,13 +5,18 @@ package com.continuecoding.videostreamingapp.service;
 
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
 /**
  * @author Ashutosh
  *
  */
 @Service
+@RequiredArgsConstructor
 public class UserRegistrationService {
 	
+	private String userInfoEndpoint;
 	
+	private final UserRepository userRepository;
 
 }
