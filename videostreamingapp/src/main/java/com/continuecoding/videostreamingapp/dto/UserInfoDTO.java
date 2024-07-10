10a88429d5ed52ad380324e94c455c7f406fe81c
Ashutosh <ashutosh.mahato@gmail.com>
2024-07-10 17:59:57 +0530

    Added json property for family name

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/dto/UserInfoDTO.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/dto/UserInfoDTO.java
@@ -8,9 +8,9 @@ public class UserInfoDTO {
 	private String id;
 	@JsonProperty("sub")
 	private String sub;
-	
+	@JsonProperty("given_name")
 	private String givenName;
-	
+	@JsonProperty("family_name")
 	private String familyName;
 	
 	private String name;
