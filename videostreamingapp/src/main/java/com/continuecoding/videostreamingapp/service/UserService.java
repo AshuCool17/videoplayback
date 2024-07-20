9461e9dd9e66a14338222618f34920ca1875ff43
Ashutosh <ashutosh.mahato@gmail.com>
2024-07-20 12:39:57 +0530

    Added UserService class

---
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/UserService.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.continuecoding.videostreamingapp.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Ashutosh
+ *
+ */
+@Service
+public class UserService {
+
+}
