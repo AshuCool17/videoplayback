b0e1f87f243b514e92bc2bd8fa0c8f04c21cacac
Ashutosh <ashutosh.mahato@gmail.com>
2024-07-20 12:41:17 +0530

    Added userrepo field

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/UserService.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/UserService.java
@@ -5,11 +5,19 @@ package com.continuecoding.videostreamingapp.service;
 
 import org.springframework.stereotype.Service;
 
+import com.continuecoding.videostreamingapp.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
 /**
  * @author Ashutosh
  *
  */
 @Service
+@RequiredArgsConstructor
 public class UserService {
 
+	private final UserRepository userRepository;
+	
+	public User
 }
