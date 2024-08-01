ee093db15e6f2cf1dc8538c383601124dc51a183
Ashutosh <ashutosh.mahato@gmail.com>
2024-08-01 10:23:13 +0530

    Added service for disLikeVideo

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/VideoService.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/service/VideoService.java
@@ -110,4 +110,8 @@ public class VideoService {
 		videoDto.setLikeCount(videoById.getLikes().get());
 		videoDto.setDislikeCount(videoById.getDislikes().get());
 	}
+
+	public VideoDto disLikeVideo(String videoId) {
+		return null;
+	}
 }
