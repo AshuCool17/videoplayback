0a688b4c6a58203ad2b35d2fbd2a17fe8eb5df6d
Ashutosh <ashutosh.mahato@gmail.com>
2024-08-17 22:16:41 +0530

    Fixed import issue

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/controller/UserController.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.continuecoding.videostreamingapp.service.UserRegistrationService;
+import com.continuecoding.videostreamingapp.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
