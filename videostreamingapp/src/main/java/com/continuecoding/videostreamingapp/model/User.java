1a4671fe56684f906b070ccdd33b51feaa6765a6
Ashutosh <ashutosh.mahato@gmail.com>
2024-08-26 23:14:43 +0530

    Renamed variable to userId

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/model/User.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/model/User.java
@@ -75,4 +75,8 @@ public class User {
 	public void removeFromSubscribedToUsers(String userId) {
 		subscribers.remove(userId);
 	}
+
+	public void removeFromUnsubscribers(String userId) {
+		
+	}
 }
