8dfb849cc202b246c061c5e56b92720b7e2f99cc
Ashutosh <ashutosh.mahato@gmail.com>
2024-09-02 17:30:39 +0530

    Added getter setter annotation

--- videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/dto/CommentDto.java
+++ videostreamingapp/src/main/java/com/continuecoding/videostreamingapp/dto/CommentDto.java
@@ -3,10 +3,15 @@
  */
 package com.continuecoding.videostreamingapp.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author Ashutosh
  *
  */
+@Setter
+@Getter
 public class CommentDto {
 
 }
